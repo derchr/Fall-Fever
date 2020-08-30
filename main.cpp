821b677ff94d9e7408ba21804880fc0ed868d419
4VRDriver <44267643+4VRDriver@users.noreply.github.com>
2020-08-30 21:24:39 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char** argv) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW-Window", NULL, NULL);
+    
+    while(1){}
+    
+    return 0;
+}
